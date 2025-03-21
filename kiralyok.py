2481d99bf71b450bd9548d83f7a5fe91f5dba897
sereszalan <seres.zalan@diak.szbi-pg.hu>
2025-03-21 13:30:48 +0100

    Mátyás király adatai

--- kiralyok.py
+++ kiralyok.py
@@ -3,10 +3,28 @@ from mysql.connector import (connection)
 cnx = connection.MySQLConnection(user='root', password='mysql',
                                  host='127.0.0.1',
                                  database='kiralyok')
+#táblák megjelenítése
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+print(-----------------------------)
 
+#uralkodó megjelenítése
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+print(-----------------------------)
+
+#mátyás
+cursor.execute("SELECT * FROM uralkodo WHERE nev ='I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)
+print(------------------------------)
+
+#Mátyás király adatai
+cursor.execute("SELECT szul, hal FROM uralkodo WHERE nev ='I. Mátyás'")
+for uralkodo in cursor:
+    print(f"Mátyás király élt: {uralkodo[0]}-{uralkodo[1]}")
 
 cnx.close()
